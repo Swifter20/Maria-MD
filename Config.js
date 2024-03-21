34e01e955f43bc381f097b1c2c4fd39cf98e19fa
Swifter20 <164353906+Swifter20@users.noreply.github.com>
2024-03-21 16:47:44 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,12 +3,12 @@ const chalk = require('chalk')
 
 //contact details
 global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownername = "💙Swifter✰"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
 
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.botname = 'Swift.Mech' //name of the bot
 
 //sticker details
 global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
